7d1ea9b9be7d05e47d77424c52b9f2daf3f1bec5
Kidus Geremew <kidusg98@gmail.com>
2026-02-06 17:09:49 +0300

    adding index html file for scraping

--- mainTest/main.go
+++ mainTest/main.go
@@ -9,17 +9,16 @@ import (
 func main() {
 	fmt.Println("Creating.....")
 	collector := colly.NewCollector(
-		colly.AllowedDomains("guides.rubyonrails.org", "www.guides.rubyonrails.org"),
+		colly.AllowedDomains("127.0.0.1"),
 	)
 
-	collector.OnHTML("header .wrapper ul li", func(h *colly.HTMLElement) {
+	collector.OnHTML(".category-item", func(h *colly.HTMLElement) {
 		fmt.Println("tag name: ", h.Name, " index: ", h.Index, " text: ", h.Text)
-
 	})
 
 	collector.OnRequest(func(r *colly.Request) {
 		fmt.Println("Visiting: ", r.URL.String())
 	})
 
-	collector.Visit("https://guides.rubyonrails.org/active_record_validations.html")
+	collector.Visit("http://127.0.0.1:5500/mainTest/index.html")
 }
